c3c352ee19eb39b84aca80c87e13f89fb466fed8
penelopeundercover <mjseiwert@outlook.com>
2022-07-26 18:11:44 -0500

    tuesday evening

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,48 @@
-var searchInputElement = document.getElementById("searchCityInput");
-var searchButtonElement = document.getElementById("submit-search-button");
+var searchInputElement = document.getElementById("enterCity");
+var searchButtonElement = document.getElementById("searchBtn");
 var currentWeatherContainerElement = document.getElementById(
   "current-weather-container"
 );
 
 var WEATHER_API_KEY = "c7a5b1daca5f74e9896820a90d4e53e8";
 
-function getCoordinates(city) {
-  var coordinates = {
-    long: "",
-    lat: "",
-  };
-  fetch(
-    "http://api.openweathermap.org/geo/1.0/direct?appid=${WEATHER_API_KEY}q=${city}&limit=1"
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
+//When user clicks the Search button, save user input
+searchButtonElement.addEventListener("click", function () {
+  console.log(searchInputElement.value);
+});
 
-  //Do something to get it
-  return coordinates;
-}
+//Get weather info from the openweather app
+fetch(
+  `https://api.openweathermap.org/data/2.5/forecast?q=portland&appid=${WEATHER_API_KEY}`
+)
+  .then(function (response) {
+    console.log(response);
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+
+// function getCoordinates(city) {
+//   var coordinates = {
+//     long: "",
+//     lat: "",
+//   };
+//   fetch(
+//     "http://api.openweathermap.org/geo/1.0/direct?appid=${WEATHER_API_KEY}q=${city}&limit=1"
+//   )
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//     });
+
+//   //Do something to get it
+//   return coordinates;
+// }
+
+//Display the weather info we just fetched
 
 function handleSearch(event) {
   event.preventDefault();
