009f7ebbad08674ee1133f76b8c4360f3b2cf47a
penelopeundercover <mjseiwert@outlook.com>
2022-07-26 13:59:57 -0500

    tuesdayafternoon

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,30 @@
+var searchInputElement = document.getElementById("searchCityInput");
+var searchButtonElement = document.getElementById("submit-search-button");
+var currentWeatherContainerElement = document.getElementById(
+  "current-weather-container"
+);
+
 var WEATHER_API_KEY = "c7a5b1daca5f74e9896820a90d4e53e8";
-var coordinates = {
+
+function getCoordinates(city) {
+  var coordinates = {
     long: "",
-    lat: ""
+    lat: "",
+  };
+  fetch(
+    "http://api.openweathermap.org/geo/1.0/direct?appid=${WEATHER_API_KEY}q=${city}&limit=1"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+
+  //Do something to get it
+  return coordinates;
 }
-fetch("http://api.openweathermap.org/geo/1.0/direct?q={city name},{state
-    code},{country code}&limit={limit}&appid=${WEATHER_API_KEY}");
-    then(function(response)) { then return response.json();
-    }).then(function(data) { console.log(data); });
+
+function handleSearch(event) {
+  event.preventDefault();
+}
